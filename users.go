d38ef9573415f4829431a813c5a9f51680a9c32f
Jonathan Hurter <john@scalingo.com>
2017-12-06 14:43:23 +0100

    Restore authentication_token and add UUID

--- users.go
+++ users.go
@@ -3,12 +3,14 @@ package scalingo
 import "gopkg.in/errgo.v1"
 
 type User struct {
-	ID        string          `json:"id"`
-	Username  string          `json:"username"`
-	FirstName string          `json:"first_name"`
-	LastName  string          `json:"last_name"`
-	Email     string          `json:"email"`
-	Flags     map[string]bool `json:"flags"`
+	ID                  string          `json:"id"`
+	UUID                string          `json:"uuid"`
+	Username            string          `json:"username"`
+	FirstName           string          `json:"first_name"`
+	LastName            string          `json:"last_name"`
+	Email               string          `json:"email"`
+	Flags               map[string]bool `json:"flags"`
+	AuthenticationToken string          `json:"authentication"`
 }
 
 type SelfResponse struct {
