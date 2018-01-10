2e5ec0a08f3a910b81c31f280f9bde67fb10adb2
Étienne Michon <etienne@scalingo.com>
2018-01-10 17:39:32 +0100

    Fix tests

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Generate the mocks with:
 
 ```shell
-for interface in $(grep --extended-regexp --no-message --no-filename "type (.*Service|API) interface" ./* | grep -v  mockgen | cut -d " " -f 2)
+for interface in $(grep --extended-regexp --no-message --no-filename "type (.*Service|API|TokenGenerator) interface" ./* | grep -v  mockgen | cut -d " " -f 2)
 do
   mockgen -destination scalingomock/gomock_$(echo $interface | tr '[:upper:]' '[:lower:]').go -package scalingomock github.com/Scalingo/go-scalingo $interface
 done
--- events_types_test.go
+++ events_types_test.go
@@ -2,39 +2,43 @@ package scalingo
 
 import (
 	"encoding/json"
-	"reflect"
+	"fmt"
 	"testing"
 )
 
-var eventsSpecializeCases = []struct {
+var eventsSpecializeCases = map[string]struct {
 	Event *Event
 
 	// Expected
 	DetailedEventName   string
 	DetailedEventString string
-}{{
-	Event: &Event{
-		User: EventUser{
-			Username: "user1",
+}{
+	"test event specialization": {
+		Event: &Event{
+			User: EventUser{
+				Username: "user1",
+			},
+			Type:        EventRestart,
+			RawTypeData: json.RawMessage([]byte(`{"scope": ["web"]}`)),
 		},
-		Type:        EventRestart,
-		RawTypeData: json.RawMessage([]byte(`{"scope": "web"}`)),
+		DetailedEventName:   "*scalingo.EventRestartType",
+		DetailedEventString: "containers [web] have been restarted",
 	},
-	DetailedEventName:   "EventRestartType",
-	DetailedEventString: "user1 restarted 'web' containers",
-}}
+}
 
 func TestEvent_Specialize(t *testing.T) {
-	for _, c := range eventsSpecializeCases {
-		dev := c.Event.Specialize()
+	for msg, c := range eventsSpecializeCases {
+		t.Run(msg, func(t *testing.T) {
+			dev := c.Event.Specialize()
 
-		tdev := reflect.ValueOf(dev).Elem().Type().Name()
-		if tdev != c.DetailedEventName {
-			t.Errorf("Expecting event of type %v, got %v", c.DetailedEventName, tdev)
-		}
+			tdev := fmt.Sprintf("%T", dev)
+			if tdev != c.DetailedEventName {
+				t.Errorf("Expecting event of type %v, got %v", c.DetailedEventName, tdev)
+			}
 
-		if dev.String() != c.DetailedEventString {
-			t.Errorf("Expecting event string\n===\n%s\n=== got\n%s\n===", c.DetailedEventString, dev.String())
-		}
+			if dev.String() != c.DetailedEventString {
+				t.Errorf("Expecting event string\n===\n%s\n=== got\n%s\n===", c.DetailedEventString, dev.String())
+			}
+		})
 	}
 }
