3f9726c5001bee4741a312325bdd2dcdb618fd45
Étienne Michon <etienne@scalingo.com>
2018-01-17 10:37:44 +0100

    Add documentation

--- static_token.go
+++ static_token.go
@@ -1,20 +1,26 @@
 package scalingo
 
+// StaticTokenGenerator is an implementation of TokenGenerator which always return the same token.
+// This token is provided in the GetStaticTokenGenerator function.
 type StaticTokenGenerator struct {
 	token  string
 	client *Client
 }
 
+// GetStaticTokenGenerator returns a new StaticTokenGenerator. The only argument is the token that
+// will always be returned by this generator.
 func GetStaticTokenGenerator(token string) *StaticTokenGenerator {
 	return &StaticTokenGenerator{
 		token: token,
 	}
 }
 
+// GetAccessToken always returns the configured token.
 func (t *StaticTokenGenerator) GetAccessToken() (string, error) {
 	return t.token, nil
 }
 
+// SetClient sets the client attribute of this token generator.
 func (t *StaticTokenGenerator) SetClient(c *Client) {
 	t.client = c
 }
