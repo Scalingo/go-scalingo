571a1c75830b27eae56fe2c3d5cff5dba71e3afe
Étienne Michon <etienne@scalingo.com>
2019-07-16 11:06:32 +0200

    Add linker_id to the GitHub link

--- github_link.go
+++ github_link.go
@@ -28,6 +28,7 @@ type GithubLinkParams struct {
 type GithubLink struct {
 	ID                       string           `json:"id"`
 	Linker                   GithubLinkLinker `json:"linker"`
+	LinkerID                 string           `json:"linker_id"`
 	CreatedAt                time.Time        `json:"created_at"`
 	UpdatedAt                time.Time        `json:"updated_at"`
 	GithubSource             string           `json:"github_source"`
