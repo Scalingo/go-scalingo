cdb6d2764b3d8d941af4a921ffc88d8169a4a6f1
CURZOLA Pierre <pierre.curzola@gmail.com>
2020-04-23 16:03:03 +0200

    list works with the current implementation of log drains

--- log_drains.go
+++ log_drains.go
@@ -15,6 +15,14 @@ type LogDrain struct {
 	URL   string `json:"url"`
 }
 
+type LogDrainsRes struct {
+	LogDrains []LogDrain
+}
+
+type LogDrainRes struct {
+	LogDrain LogDrain `json:"log_drain"`
+}
+
 func (c *Client) LogDrainsList(app string) ([]LogDrain, error) {
 	var logDrainsRes []LogDrain
 	err := c.ScalingoAPI().SubresourceList("apps", app, "log_drains", nil, &logDrainsRes)
@@ -22,4 +30,46 @@ func (c *Client) LogDrainsList(app string) ([]LogDrain, error) {
 		return nil, errgo.Notef(err, "fail to list the log drains")
 	}
 	return logDrainsRes, nil
+}
+
+type LogDrainAddParams struct {
+	// Type      string `json:"drain_type"`
+	// Status    string `json:"status"`
+	// TargetURL string `json:"targetURL"`
+	AppID string `json:"app_id"`
+	URL   string `json:"url"`
+}
+
+func (c *Client) LogDrainAdd(app string, params LogDrainAddParams) (*LogDrain, error) {
+	var logDrainRes LogDrainRes
+	err := c.ScalingoAPI().SubresourceAdd("apps", app, "log_drains", LogDrainRes{
+		LogDrain: LogDrain{
+			// Type:      params.Type,
+			// TargetURL: params.TargetURL,
+			// Status:    params.Status,
+			AppID: params.AppID,
+			URL:   params.URL,
+		},
+	}, &logDrainRes)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	return &logDrainRes.LogDrain, nil
+}
+
+func (c *Client) LogDrainInfo(app, id string) (*LogDrain, error) {
+	var logDrainRes LogDrainRes
+	err := c.ScalingoAPI().SubresourceGet("apps", app, "log_drains", id, nil, &logDrainRes)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	return &logDrainRes.LogDrain, nil
+}
+
+func (c *Client) LogDrainRemove(app, id string) error {
+	err := c.ScalingoAPI().SubresourceDelete("apps", app, "log_drains", id)
+	if err != nil {
+		return errgo.Mask(err)
+	}
+	return nil
 }
