53ddd9792399e7111e15abe4f9448cf2bbe93799
CURZOLA Pierre <pierre.curzola@gmail.com>
2020-05-06 13:27:05 +0200

    Add changelog entry. Remove vscode config from git. Remove unused functions

--- log_drains.go
+++ log_drains.go
@@ -6,8 +6,6 @@ import (
 
 type LogDrainsService interface {
 	LogDrainsList(app string) ([]LogDrain, error)
-	LogDrainAdd(app string, params LogDrainAddParams) (*LogDrain, error)
-	LogDrainRemove(app string, id string) error
 }
 
 var _ LogDrainsService = (*Client)(nil)
@@ -21,10 +19,6 @@ type LogDrainsRes struct {
 	LogDrains []LogDrain
 }
 
-type LogDrainRes struct {
-	LogDrain LogDrain `json:"log_drain"`
-}
-
 func (c *Client) LogDrainsList(app string) ([]LogDrain, error) {
 	var logDrainsRes []LogDrain
 	err := c.ScalingoAPI().SubresourceList("apps", app, "log_drains", nil, &logDrainsRes)
@@ -32,40 +26,4 @@ func (c *Client) LogDrainsList(app string) ([]LogDrain, error) {
 		return nil, errgo.Mask(err)
 	}
 	return logDrainsRes, nil
-}
-
-type LogDrainAddParams struct {
-	AppID string `json:"app_id"`
-	URL   string `json:"url"`
-}
-
-func (c *Client) LogDrainAdd(app string, params LogDrainAddParams) (*LogDrain, error) {
-	var logDrainRes LogDrainRes
-	err := c.ScalingoAPI().SubresourceAdd("apps", app, "log_drains", LogDrainRes{
-		LogDrain: LogDrain{
-			AppID: params.AppID,
-			URL:   params.URL,
-		},
-	}, &logDrainRes)
-	if err != nil {
-		return nil, errgo.Mask(err)
-	}
-	return &logDrainRes.LogDrain, nil
-}
-
-func (c *Client) LogDrainInfo(app, id string) (*LogDrain, error) {
-	var logDrainRes LogDrainRes
-	err := c.ScalingoAPI().SubresourceGet("apps", app, "log_drains", id, nil, &logDrainRes)
-	if err != nil {
-		return nil, errgo.Mask(err)
-	}
-	return &logDrainRes.LogDrain, nil
-}
-
-func (c *Client) LogDrainRemove(app, id string) error {
-	err := c.ScalingoAPI().SubresourceDelete("apps", app, "log_drains", id)
-	if err != nil {
-		return errgo.Mask(err)
-	}
-	return nil
 }
