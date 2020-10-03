359f2e19c1cdb3a46e23406439bed6cd9db3f95b
Soulou <leo@unbekandt.eu>
2020-10-03 10:34:16 +0200

    Make API version prefix more easily configurable by user
    Currently changing the /v1 prefix for the API endpoint was impossible but was required for internal usages of the API, this PR allows this

--- client.go
+++ client.go
@@ -54,8 +54,11 @@ type ClientConfig struct {
 	Timeout             time.Duration
 	TLSConfig           *tls.Config
 	APIEndpoint         string
+	APIPrefix           string
 	AuthEndpoint        string
+	AuthPrefix          string
 	DatabaseAPIEndpoint string
+	DatabaseAPIPrefix   string
 	APIToken            string
 	Region              string
 	UserAgent           string
@@ -113,12 +116,16 @@ func (c *Client) ScalingoAPI() http.Client {
 	if len(c.config.APIToken) != 0 {
 		tokenGenerator = http.NewAPITokenGenerator(c, c.config.APIToken)
 	}
+	prefix := "/v1"
+	if c.config.APIPrefix != "" {
+		prefix = c.config.APIPrefix
+	}
 
 	return http.NewClient(http.ScalingoAPI, http.ClientConfig{
 		UserAgent:      c.config.UserAgent,
 		Timeout:        c.config.Timeout,
 		TLSConfig:      c.config.TLSConfig,
-		APIVersion:     "1",
+		APIConfig:      http.APIConfig{Prefix: prefix},
 		TokenGenerator: tokenGenerator,
 		Endpoint:       c.config.APIEndpoint,
 	})
@@ -128,9 +135,15 @@ func (c *Client) DBAPI(app, addon string) http.Client {
 	if c.dbClient != nil {
 		return c.dbClient
 	}
+
+	prefix := "/api"
+	if c.config.DatabaseAPIPrefix != "" {
+		prefix = c.config.DatabaseAPIPrefix
+	}
 	return http.NewClient(http.DBAPI, http.ClientConfig{
 		Timeout:        c.config.Timeout,
 		TLSConfig:      c.config.TLSConfig,
+		APIConfig:      http.APIConfig{Prefix: prefix},
 		TokenGenerator: http.NewAddonTokenGenerator(app, addon, c),
 		Endpoint:       c.config.DatabaseAPIEndpoint,
 	})
@@ -149,10 +162,14 @@ func (c *Client) AuthAPI() http.Client {
 		tokenGenerator = http.NewAPITokenGenerator(c, c.config.APIToken)
 	}
 
+	prefix := "/v1"
+	if c.config.AuthPrefix != "" {
+		prefix = c.config.AuthPrefix
+	}
 	return http.NewClient(http.AuthAPI, http.ClientConfig{
 		Timeout:        c.config.Timeout,
 		TLSConfig:      c.config.TLSConfig,
-		APIVersion:     "1",
+		APIConfig:      http.APIConfig{Prefix: prefix},
 		TokenGenerator: tokenGenerator,
 		Endpoint:       c.config.AuthEndpoint,
 	})
