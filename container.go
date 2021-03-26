890475f9b91310daf1c67314286d8f348981bd63
Étienne Michon <etienne@scalingo.com>
2021-03-26 17:05:00 +0100

    feat(container): add ContainerSize to the Container struct

--- container.go
+++ container.go
@@ -24,8 +24,24 @@ type Container struct {
 	TypeIndex int        `json:"type_index"`
 	Label     string     `json:"label"`
 	State     string     `json:"state"`
-	Size      string     `json:"size"`
 	App       *App       `json:"app"`
+	// Size has been deprecated in favor of the more comprehensive `ContainerSize` attribute
+	Size          string        `json:"size"`
+	ContainerSize ContainerSize `json:"container_size"`
+}
+
+type ContainerSize struct {
+	ID              string `json:"id"`
+	HumanCPU        string `json:"human_cpu"`
+	Name            string `json:"name"`
+	HumanName       string `json:"human_name"`
+	Memory          int64  `json:"memory"`
+	Ordinal         int    `json:"ordinal"`
+	HourlyPrice     int    `json:"hourly_price"`
+	ThirtydaysPrice int    `json:"thirtydays_price"`
+	PidsLimit       int    `json:"pids_limit,omitempty"`
+	Swap            int64  `json:"swap"`
+	SKU             string `json:"sku,omitempty"`
 }
 
 func (c *Client) ContainersStop(appName, containerID string) error {
