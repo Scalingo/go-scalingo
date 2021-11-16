04a824c27136474ac430b860f4214d639e20021b
Étienne Michon <etienne@scalingo.com>
2021-11-16 17:57:01 +0100

    refactor: replace use of the deprecated ioutil package

--- http/api_request.go
+++ http/api_request.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"reflect"
@@ -145,7 +144,7 @@ func (c *client) doRequest(req *http.Request) (*http.Response, error) {
 }
 
 func parseJSON(res *http.Response, data interface{}) error {
-	body, err := ioutil.ReadAll(res.Body)
+	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		reqErr := fmt.Sprintf("%v %v", res.Request.Method, res.Request.URL)
 		return errgo.Newf("fail to read body of request %v: %v", reqErr, err)
