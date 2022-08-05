0da96b7d148396ba80ddaeb4eaaa41d25939944d
Étienne Michon <etienne@scalingo.com>
2022-08-05 10:54:21 +0200

    feat(container): remove deprecated Size

--- container.go
+++ container.go
@@ -17,18 +17,16 @@ type ContainersService interface {
 var _ ContainersService = (*Client)(nil)
 
 type Container struct {
-	ID        string     `json:"id"`
-	AppID     string     `json:"app_id"`
-	CreatedAt *time.Time `json:"created_at"`
-	DeletedAt *time.Time `json:"deleted_at"`
-	Command   string     `json:"command"`
-	Type      string     `json:"type"`
-	TypeIndex int        `json:"type_index"`
-	Label     string     `json:"label"`
-	State     string     `json:"state"`
-	App       *App       `json:"app"`
-	// Size has been deprecated in favor of the more comprehensive `ContainerSize` attribute
-	Size          string        `json:"size"`
+	ID            string        `json:"id"`
+	AppID         string        `json:"app_id"`
+	CreatedAt     *time.Time    `json:"created_at"`
+	DeletedAt     *time.Time    `json:"deleted_at"`
+	Command       string        `json:"command"`
+	Type          string        `json:"type"`
+	TypeIndex     int           `json:"type_index"`
+	Label         string        `json:"label"`
+	State         string        `json:"state"`
+	App           *App          `json:"app"`
 	ContainerSize ContainerSize `json:"container_size"`
 }
 
