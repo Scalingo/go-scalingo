e09c62cff8ba043e0e8e469b6b4c1561c27c19e8
Cédric Thomas <cedric.thomas@scalingo.com>
2022-09-14 11:28:30 +0200

    feat(invoices-service): list + get

---
+++ invoices.go
@@ -0,0 +1,68 @@
+package scalingo
+
+import (
+	"context"
+	"time"
+
+	"gopkg.in/errgo.v1"
+)
+
+type InvoicesService interface {
+	ListInvoices(context.Context, PaginationOpts) (Invoices, PaginationMeta, error)
+	InvoiceShow(context.Context, string) (*Invoice, error)
+}
+
+var _ InvoicesService = (*Client)(nil)
+
+type Invoice struct {
+	ID                string    `json:"id"`
+	TotalPrice        int       `json:"total_price"`
+	TotalPriceWithVat int       `json:"total_price_with_vat"`
+	BillingMonth      time.Time `json:"billing_month"`
+	PdfURL            string    `json:"pdf_url"`
+	InvoiceNumber     string    `json:"invoice_number"`
+	State             string    `json:"state"`
+	VatRate           int       `json:"vat_rate"`
+	Items             []struct {
+		ID    string `json:"id"`
+		Label string `json:"label"`
+		Price string `json:"price"`
+	} `json:"items"`
+	DetailedItems []struct {
+		ID    string `json:"id"`
+		Label string `json:"label"`
+		Price string `json:"price"`
+		App   string `json:"app"`
+	} `json:"detailed_items"`
+}
+
+type Invoices []*Invoice
+
+type InvoicesRes struct {
+	Invoices Invoices `json:"invoices"`
+	Meta     struct {
+		PaginationMeta PaginationMeta `json:"pagination"`
+	}
+}
+
+type InvoiceRes struct {
+	Invoice *Invoice `json:"invoice"`
+}
+
+func (c *Client) ListInvoices(ctx context.Context, opts PaginationOpts) (Invoices, PaginationMeta, error) {
+	var invoicesRes InvoicesRes
+	err := c.ScalingoAPI().ResourceList(ctx, "account/invoices", opts.ToMap(), &invoicesRes)
+	if err != nil {
+		return nil, PaginationMeta{}, errgo.Mask(err)
+	}
+	return invoicesRes.Invoices, invoicesRes.Meta.PaginationMeta, nil
+}
+
+func (c *Client) InvoiceShow(ctx context.Context, id string) (*Invoice, error) {
+	var invoiceRes InvoiceRes
+	err := c.ScalingoAPI().ResourceGet(ctx, "account/invoices", id, nil, &invoiceRes)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	return invoiceRes.Invoice, nil
+}
