9cb0f113d556fb0f1d0d33e0182c771df5687cf8
TheoPouletSouchard <theo.poulet.souchard@scalingo.com>
2025-06-26 12:10:39 +0200

    fix user enumeration in event

--- events_structs.go
+++ events_structs.go
@@ -447,7 +447,7 @@ func (ev *EventChangeCollaboratorRoleType) String() string {
 		role = "Limited collaborator"
 	}
 
-	if ev.TypeData.Collaborator.Username == "" {
+	if ev.TypeData.Collaborator.Username == "n/a" {
 		return fmt.Sprintf("%s is now a %s", ev.TypeData.Collaborator.Email, role)
 	}
 
