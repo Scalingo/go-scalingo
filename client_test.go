95c973a13031d1e1a6bb3afd7ba28f00f347bd95
aurelien-reeves-scalingo <aurelien.reeves@scalingo.com>
2026-02-12 15:51:37 +0100

    fix(lint): Use canonical header key

--- client_test.go
+++ client_test.go
@@ -68,14 +68,14 @@ func TestNewClient(t *testing.T) {
 				_, err := w.Write([]byte(`{"user": {}}`))
 				require.NoError(t, err)
 			}
-			assert.Equal(t, r.Header.Get("foo"), "bar")
+			assert.Equal(t, "bar", r.Header.Get("Foo"))
 		}))
 		defer authserver.Close()
 
 		client, err := New(ctx, ClientConfig{
 			AuthEndpoint: authserver.URL,
 			APIToken:     "api-token",
-			ExtraHeaders: http.Header{"foo": {"bar"}},
+			ExtraHeaders: http.Header{"Foo": {"bar"}},
 		})
 		require.NoError(t, err)
 
