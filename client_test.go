0b823b5a164773f617eca37919bda82e8119da45
Aurélien Reeves <111763415+aurelien-reeves-scalingo@users.noreply.github.com>
2026-02-13 09:42:04 +0100

    [STORY-3234] feat(http-client): Add possibility to add extra headers to HTTP requests (#473)
    * feat(http-client): Add possibility to add extra headers to HTTP requests

--- client_test.go
+++ client_test.go
@@ -68,12 +68,14 @@ func TestNewClient(t *testing.T) {
 				_, err := w.Write([]byte(`{"user": {}}`))
 				require.NoError(t, err)
 			}
+			assert.Equal(t, "bar", r.Header.Get("Foo"))
 		}))
 		defer authserver.Close()
 
 		client, err := New(ctx, ClientConfig{
 			AuthEndpoint: authserver.URL,
 			APIToken:     "api-token",
+			ExtraHeaders: http.Header{"Foo": {"bar"}},
 		})
 		require.NoError(t, err)
 
